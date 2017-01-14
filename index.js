fd65dbe0d18930a8208b607c341cae519dfcd260
Petras Alexandra <anda.petras@gmail.com>
2017-01-14 12:39:24 +0200

    added  intercom authorization button

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 var express = require('express');
 var app = express();
 
+var passport = require('passport');
+var flash = require('connect-flash');
+
+
+var IntercomStrategy = require('passport-intercom').Strategy;
+
+app.use(flash());
+
+passport.use(new IntercomStrategy({
+    clientID: 'e3d7ab13-742a-4a05-baf1-f246a38bd182',
+    clientSecret: '2aae64b4-3af2-44e8-8366-32f865fdae77',
+    callbackURL: "https://tinymetrics.in/auth/redirect"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    console,log("accessToken "+accessToken+" profile "+profile);
+  }
+));
+  
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -9,12 +28,33 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+
+
 app.get('/', function(request, response) {
+
   response.render('pages/index');
 });
+app.get('/register', function(request, response) {
+	
+  response.render('pages/register');
+});
+app.get('/registerAuth', function(request, response) {
+  
+  response.render('pages/registerAuth');
+});
+app.get('/auth/intercom',
+  passport.authenticate('intercom'));
+
+app.get('/auth/intercom/callback',
+  passport.authenticate('intercom', { failureRedirect: '/register' }),
+  function(req, res) {
+  	console.log('res' + res);
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
 
