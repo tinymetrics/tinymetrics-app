9cdb880e6997e5938d96c2f442ab17dfb02315c6
Petras Alexandra <anda.petras@gmail.com>
2017-01-19 19:50:23 +0200

    added config file

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  intercom: {
+    clientID: 'e3d7ab13-742a-4a05-baf1-f246a38bd182',
+    clientSecret: '2aae64b4-3af2-44e8-8366-32f865fdae77',
+    callbackURL: "https://tinymetricsdev.herokuapp.com/auth/redirect"
+  }
+};
