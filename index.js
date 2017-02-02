d70e16a2bb4b58978cb3cf05c8682284f6014cbf
Petras Alexandra <anda.petras@gmail.com>
2017-02-02 21:58:12 +0200

    added session to keep token, modified home and register pages

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ var passport = require('passport');
 var config = require('./config.js');
 var requestHttp = require('request');
 var Intercom = require('intercom-client');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+
+
+
+app.use(cookieParser())
+app.use(session({secret: '1234%asd'}));
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
 
 
 var IntercomStrategy = require('passport-intercom').Strategy;
@@ -19,7 +29,6 @@ passport.use(new IntercomStrategy({
   }
 ));
 
-
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -30,12 +39,40 @@ app.set('view engine', 'ejs');
 
 
 app.get('/', function(request, response) {
+  if(request.query.token!=null){
+    request.session.token= request.query.token;
+  }
+  // set hardcoded token for test purpose
+  // if(request.session.token!=null){
+  //   request.session.token = 'dG9rOjZkMzhlNTJjX2QzMjFfNDZmNV9hMGY0XzllYTgzYjQ2OGY1ZjoxOjA=';  
+  // }
   response.render('pages/index');
 });
 
 app.get('/register', function(request, response) {
+     response.render('pages/register');
+});
 
-  response.render('pages/register');
+app.post('/saveUser',function(request,response){
+
+      requestHttp.post('https://secret-waters-92571.herokuapp.com/user',
+      { json: { accessToken: request.session.token ,
+       fullName:request.body.fullName,
+       email:request.body.email
+              } },
+      function (error, resp, body) {
+          console.log("sentData");
+          //console.log(resp);
+          response.json('ok');
+        });
+});
+
+app.get('/intercomUserData',function(request,response){
+   var client = new Intercom.Client({ token: request.session.token});
+   client.admins.me(function(usr){
+     //console.log(usr.body);
+      response.json({ data: usr}); 
+    });
 });
 
 app.get('/registerAuth', function(request, response) {
@@ -54,77 +91,71 @@ app.get('/auth/intercom/callback',
   });
 
 app.get('/auth/redirect',  function(request, response) {
-
-
-    console.log("CODE:"+request.query.code);
- 
-  //  var myJSONObject = { 
-  //   code:request.query.code,
-  //   client_id:config.intercom.clientID,
-  //   client_secret:config.intercom.clientSecret
-  //  };
-
-
-
-
-  // requestHttp({
-  //     url: "https://api.intercom.io/auth/eagle/token",
-  //     method: "POST",
-  //     json: true,   // <--Very important!!!
-  //     body: myJSONObject
-  // }, function (error, res){
-  //   console.log("DONEEEE");
+    requestHttp.post(
+      'https://api.intercom.io/auth/eagle/token',
+      { json: { code:request.query.code,
+       client_id:config.intercom.clientID,
+       client_secret:config.intercom.clientSecret
+       } },
+      function (error, resp, body) {
+          if (!error && resp.statusCode == 200) {
      
-  //   console.log(JSON.parse(res));
-  //     var client = new Intercom.Client({ token: res.token });
-  //     console.log("clienyt"+client);
-  //      client.users.list(function (d) {
-  //         console.log("clienUsers");
-  //         console.log(d);
-  //       });
+             console.log(body.token);
+              request.session.token = body.token;
 
-  //       response.redirect('/');
+              var client = new Intercom.Client({ token: body.token });
+               client.users.list(function (d) {
+                  console.log("clienUsers");
+                  console.log(d);
+                });
 
-  // });
+               client.admins.me(function(usr){
+                 console.log("USR");
+                 console.log(usr);
+               });
+                  response.redirect('/register?token=' + body.token);
+          }
+      }
+  );
 
-  requestHttp.post(
-    'https://api.intercom.io/auth/eagle/token',
-    { json: { code:request.query.code,
-     client_id:config.intercom.clientID,
-     client_secret:config.intercom.clientSecret
-     } },
-    function (error, resp, body) {
-        if (!error && resp.statusCode == 200) {
-            console.log(body);
-           console.log(body.token);
+});
 
-            var client = new Intercom.Client({ token: body.token });
-             client.users.list(function (d) {
-                console.log("clienUsers");
-                console.log(d);
-              });
-             
-             client.admins.me(function(usr){
-               console.log("USR");
-               console.log(usr);
-             });
+app.get('/counts',function(request,response){
+  var client = new Intercom.Client({ token: request.session.token});
+ client.admins.me(function(usr){
+               //  console.log("USR");
+                 //ßconsole.log(usr);
+               });
+  
+  client.users.listBy({"created_since":21},function(err, res21){
+
+    var usersCreated21 = res21.toJSON().body.total_count;
+    client.users.listBy({"created_since":14},function(err, res14){
+      var usersCreated14 = res14.toJSON().body.total_count;
+      client.users.listBy({"created_since":7},function(err, res7){
+        var usersCreated7 = res7.toJSON().body.total_count;
+        var usersCreatedlast7Days= usersCreated14-usersCreated7;
+        var usersCreatedLast14Days= usersCreated21-usersCreated14;
+        var differenceBetweekWeeks= usersCreatedlast7Days-usersCreatedLast14Days;
+        var percent= differenceBetweekWeeks*100/usersCreated14;
+
+       client.counts.appCounts(function(responseData){
+         // console.log(responseData.toJSON().body);
+          var data={ counts:responseData.toJSON().body, usersCreated: usersCreated7, percentUsers: percent};
+          response.json({ data: data});
+        });
+      });
+    });
+  });
+ 
+ // requestHttp({url:"https://api.intercom.io/subscriptions/", token:request.session.token },function (error, resp, body) {
+ //   // Do more stuff with 'body' here
+ //   console.log(body);
+
+ //    });
 
 
-                response.redirect('/');
-        }
-    }
-);
-
-   });
-
-//});
-
-  // passport.authenticate('intercom', { failureRedirect: '/register' }),
-  // function(req, res) {
-  //   console.log('res' + res);
-  //   // Successful authentication, redirect home.
-  //   res.redirect('/');
-  // });
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
